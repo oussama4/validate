2f759ad1a9c691cd1dc8e7b49a6a30c5d8911896
oussama zouaki <oussamazouaki4@gmail.com>
2024-12-21 16:25:49 +0100

    remove flect dependency

--- go.mod
+++ go.mod
@@ -1,11 +1,8 @@
 module github.com/oussama4/validate/v4
 
-go 1.17
+go 1.23
 
-require (
-	github.com/gobuffalo/flect v0.2.0
-	github.com/stretchr/testify v1.4.0
-)
+require github.com/stretchr/testify v1.4.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- validators/common.go
+++ validators/common.go
@@ -1,15 +1,46 @@
 package validators
 
 import (
-	"github.com/gobuffalo/flect"
+	"strings"
+	"unicode"
 )
 
 var CustomKeys = map[string]string{}
 
+func ToUnderscore(s string) string {
+	// Convert to lowercase first
+	s = strings.ToLower(s)
+
+	var result strings.Builder
+	var prevChar rune
+
+	for i, char := range s {
+		// Handle special characters and spaces
+		if !unicode.IsLetter(char) && !unicode.IsDigit(char) {
+			if prevChar != '_' {
+				result.WriteRune('_')
+			}
+			prevChar = '_'
+			continue
+		}
+
+		// Handle camelCase: insert underscore before uppercase letters
+		if i > 0 && unicode.IsUpper(char) && prevChar != '_' {
+			result.WriteRune('_')
+		}
+
+		// Write the current character
+		result.WriteRune(char)
+		prevChar = char
+	}
+
+	// Trim leading and trailing underscores
+	return strings.Trim(result.String(), "_")
+}
 func GenerateKey(s string) string {
 	key := CustomKeys[s]
 	if key != "" {
 		return key
 	}
-	return flect.Underscore(s)
+	return ToUnderscore(s)
 }
